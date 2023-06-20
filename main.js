9cff885f2c507b7835673f8533c386d53e0c1186
renato <renato@qualitermo.com.br>
2023-06-20 07:40:57 -0300

    Ajustando link da imagem

--- main.js
+++ main.js
@@ -14,11 +14,10 @@ $(document).ready(function() {
         const novoItem = $('<li style="display: none"></li>')
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem)
         $(`
-            <div class="overlay-imagem-link"
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"> 
-                Ver imagem em tamanho real
-            </a>
-        </div>
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a>
+
+            </div>
         `).appendTo(novoItem)
         $(novoItem).appendTo('ul')
         $(novoItem).fadeIn(1000)
